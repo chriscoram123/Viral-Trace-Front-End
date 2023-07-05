e8af170908172e4180194b4672d918ce891f1b90
Christopher Coram <ultraartist15@gmail.com>
2023-07-05 17:54:44 -0400

    Code layout for app pages complete

---
+++ login_system/new_account.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+
+export default function NewAccount() {
+    return (
+        <Text> Hello World </Text>
+    )
+}
+
+const styles = StyleSheet.create({
+
+
+});
