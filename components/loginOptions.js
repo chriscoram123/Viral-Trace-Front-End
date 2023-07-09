9f019632c0a8325281c3b96c0aba5554fab91190
Christopher Coram <ultraartist15@gmail.com>
2023-07-09 15:49:39 -0400

    Layout setup for tomorrow's session

---
+++ components/loginOptions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+
+export default function LoginOption() {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
