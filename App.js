e89b11226d96999e3cb6ccb185c55993ba7bdd3c
Christopher Coram <ultraartist15@gmail.com>
2023-07-09 14:54:30 -0400

    Re-organized login file

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import { LinearGradient } from 'expo-linear-gradient';
 import Navigator from './routes/loginStack'
 import { NavigationContainer } from '@react-navigation/native';
 
+import InputFields from './components/loginInputFields';
+import Header from './components/header';
+
 export default function Login({ navigation }) {
   return (
     <View style={styles.pageBackground}>
@@ -13,44 +16,10 @@ export default function Login({ navigation }) {
         style={styles.pageBackground}
       >
       {/* Header */}
-        <View style={styles.headerContainer}>
-
-          <View style={styles.imgContainer}>
-            <Image 
-              source={require('./assets/HeaderIcon.png')}
-            ></Image>
-          </View>
-          
-          <View style={styles.textContainer}>
-            <Text style={styles.headerText}>Proceed with</Text><Text style={styles.headerTextTwo}> LOGIN</Text>
-          </View>
-        </View>
+      <Header></Header>
 
       {/* Input Fields */}
-        <View style={styles.inputContainer}>
-
-            <View style={styles.emailField}>
-              <Text style={styles.emailFieldTitle}> Email Address </Text>
-              <TextInput
-                style={styles.textField}
-              />
-              <Image
-                source={require('./assets/DividerLine.png')}
-                style={styles.dividerLine}
-              />
-            </View>
-
-            <View style={styles.passwordField}>
-              <Text style={styles.passwordFieldTitle}> Password </Text>
-              <TextInput
-                style={styles.textField}
-              />
-              <Image
-                source={require('./assets/DividerLine.png')}
-                style={styles.dividerLine}
-              />
-            </View>
-        </View>
+      <InputFields></InputFields>
 
       {/* Login Options */}
         <View style={styles.loginButtonsContainer}>
@@ -117,12 +86,6 @@ const styles = StyleSheet.create({
       backgroundColor: 'purple',
       flex: 1,
     },
-    headerContainer: {
-      marginVertical: 60,
-    },
-    inputContainer: {
-      // backgroundColor: "red",
-    },
     socialButtonsContainer: {
       marginVertical: 20,
       // marginHorizontal: 10,
@@ -170,32 +133,6 @@ const styles = StyleSheet.create({
       marginVertical: 15,
     },
 
-  // Input Fields
-    emailFieldTitle: {
-      marginLeft: 15, 
-      marginBottom: 5,
-      fontSize: 16,
-      color: 'white',
-      letterSpacing: 2,
-    },
-    passwordFieldTitle: {
-      marginLeft: 16, 
-      marginBottom: 5,
-      fontSize: 18,
-      color: 'white',
-      letterSpacing: 2,
-    },
-    emailField: {
-      marginVertical: 10,
-    },
-    passwordField: {
-      marginVertical: 10,
-    },
-    dividerLine: {
-      marginLeft: 20,
-      marginTop: 8,
-    },
-
   // Buttons & Links
     loginButton: {
       backgroundColor: 'white',
@@ -208,25 +145,6 @@ const styles = StyleSheet.create({
     },
 
   // Text
-    headerText: {
-      color: 'white',
-      textAlign: "center",
-      fontSize: 25,
-      letterSpacing: 2,
-    },
-    headerTextTwo: {
-      color: 'white',
-      textAlign: "center",
-      fontSize: 25,
-      letterSpacing: 2,
-      fontWeight: 'bold',
-    },
-    textField: {
-      backgroundColor: 'rgba(0,0,0,0.2)',
-      marginHorizontal: 20,
-      borderRadius: 10,
-      height: 40,
-    },
     footerText: {
       fontSize: 18,
       color: 'white',
