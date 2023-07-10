3408d8f623c03f155009c349bfb7b5b0a66b516a
Christopher Coram <ultraartist15@gmail.com>
2023-07-10 17:43:55 -0400

    Seprate file for login options

--- components/loginOptions.js
+++ components/loginOptions.js
@@ -4,12 +4,60 @@ import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 
 export default function LoginOption() {
     return (
-        <View>
+        <View style={styles.loginButtonsContainer}>
+            <View style={styles.loginCon}>
+              {/* <LinearGradient
+                colors={['#B7D0FF', 'B7B7FF']}
+              > */}
+                <Button 
+                  title="Login"
+                  color='white'
+                />
+              {/* </LinearGradient> */}
+            </View>
             
+            <View style={styles.supportContainer}>
+              <View style={styles.forgotLink}>
+                <Button 
+                  title="Forgot?"
+                  onPress={() => navigation.navigate(ForgotAccount)
+                  }
+                />
+              </View>
+
+              <View style={styles.newUserLink}>
+                <Button 
+                  title="New User?"
+                  onPress={() => navigation.navigate(NewAccount)
+                  }
+                />
+              </View>
+            </View>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    loginButtonsContainer: {
+        marginTop: 50,
+        marginBottom: 30,
+    },
+    loginCon: {
+        backgroundColor: "#4D5C98",
+        marginHorizontal: 130,
+        height: 40,
+        borderRadius: 20,
+    },
+    supportContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        marginVertical: 15,
+    },
+    forgotLink: {
+        marginHorizontal: 35,
+    },
+    newUserLink: {
+        marginHorizontal: 20,
+    },
 });
